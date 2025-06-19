5179bb193dd0ce52aef64a487cbe9bdf63b2e5c6
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2025-06-19 21:20:39 +0100

    Refactoring

--- app/src/main/java/com/example/irregularverbs/MainActivity.kt
+++ app/src/main/java/com/example/irregularverbs/MainActivity.kt
@@ -15,8 +15,8 @@ import java.util.Locale
 private const val MAX_NUMBER_OF_PREVIOUS_VERBS = 10
 
 private const val IRREGULAR_VERBS_LIST_KEY = "IrregularVerbsList"
-private const val PREVIOUS_VERBS_ARRAY_KEY = "PreviousVerbsArray"
-private const val CURRENT_VERB_KEY = "CurrentVerb"
+private const val PREVIOUS_VERBS_INDEX_ARRAY_KEY = "PreviousVerbsIndexArray"
+private const val CURRENT_VERB_INDEX_KEY = "CurrentVerbIndex"
 private const val HAS_PAST_REVEALED_KEY = "HasPastRevealed"
 
 class MainActivity : AppCompatActivity() {
@@ -64,17 +64,19 @@ class MainActivity : AppCompatActivity() {
         previousVerbButton.setOnClickListener { onPreviousVerbButtonClick() }
         mPreviousVerbButton = previousVerbButton
 
-        mIrregularVerbs = savedInstanceState?.getParcelableArrayList(
+        val irregularVerbs = savedInstanceState?.getParcelableArrayList(
             IRREGULAR_VERBS_LIST_KEY,
             IrregularVerb::class.java,
         ) ?: readIrregularVerbs()
-        mPreviousVerbs = savedInstanceState?.getParcelableArray(
-            PREVIOUS_VERBS_ARRAY_KEY,
-            IrregularVerb::class.java,
-        )?.let { ArrayDeque(it.asList()) } ?: ArrayDeque(MAX_NUMBER_OF_PREVIOUS_VERBS)
+        mIrregularVerbs = irregularVerbs
+        val previousVerbs = ArrayDeque<IrregularVerb>(MAX_NUMBER_OF_PREVIOUS_VERBS)
+        savedInstanceState?.getIntArray(PREVIOUS_VERBS_INDEX_ARRAY_KEY)?.forEach { index ->
+            previousVerbs.addLast(irregularVerbs[index])
+        }
+        mPreviousVerbs = previousVerbs
 
-        mCurrentVerb =
-            savedInstanceState?.getParcelable(CURRENT_VERB_KEY, IrregularVerb::class.java)
+        val currentVerbIndex = savedInstanceState?.getInt(CURRENT_VERB_INDEX_KEY, -1) ?: -1
+        mCurrentVerb = if (currentVerbIndex < 0) null else irregularVerbs[currentVerbIndex]
         mHasPastRevealed = savedInstanceState?.getBoolean(HAS_PAST_REVEALED_KEY) == true
         updateVerbTensesText()
         updateMainActionButtonText()
@@ -198,8 +200,8 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         with(outState) {
             putParcelableArrayList(IRREGULAR_VERBS_LIST_KEY, mIrregularVerbs)
-            putParcelableArray(PREVIOUS_VERBS_ARRAY_KEY, mPreviousVerbs.toTypedArray())
-            putParcelable(CURRENT_VERB_KEY, mCurrentVerb)
+            putIntArray(PREVIOUS_VERBS_INDEX_ARRAY_KEY, mPreviousVerbs.mapToIntArray { it.index })
+            putInt(CURRENT_VERB_INDEX_KEY, mCurrentVerb?.index ?: -1)
             putBoolean(HAS_PAST_REVEALED_KEY, mHasPastRevealed)
         }
         super.onSaveInstanceState(outState)
